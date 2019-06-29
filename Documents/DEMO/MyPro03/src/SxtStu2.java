137090ab8bfeb9e005dd79fbfe4c8dfdf9669df2
Zhengyongzhi <mr.gandle@gmail.com>
2019-06-29 21:26:10 +0800

    修复Bug

---
+++ Documents/DEMO/MyPro03/src/SxtStu2.java
@@ -0,0 +1,50 @@
+/*
+ * 
+ * 类调用
+ * 
+ */
+
+
+class Computer{
+	String brand;
+}
+
+public class SxtStu2 {
+	
+	//属性fields
+	int id;
+	String sname;
+	int age;
+	
+	Computer comp;
+
+	//方法
+	void study() {
+		System.out.println("学习:"+comp.brand);
+	}
+	
+	void play() {
+		System.out.println("游戏");
+	}
+	
+	
+	//程序执行的入口
+	public static void main(String[]args) {
+		SxtStu2 stu = new SxtStu2();
+		stu.id=2014;
+		stu.sname="zheng";
+		stu.age=25;
+		
+		Computer c1 = new Computer() ;
+		c1.brand = "联想";
+		stu.comp = c1;
+		
+		stu.study();
+		
+	}
+	
+}
+
+
+
+
